4d57ebf1800e89fa9c1381567ea838ff4a6f518d
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2024-04-11 21:15:17 +0900

    新しいライブラリのコードにするため、一旦削除

--- lib/libclock/Chronos.cpp
+++
@@ -1,6 +0,0 @@
-#include "Chronos.h"
-
-std::time_t Chronos::now() const
-{
-    return Clock::to_time_t( Clock::now() );
-}
--- lib/libclock/ChronosMock.cpp
+++
@@ -1,7 +0,0 @@
-#include "ChronosMock.h"
-
-std::time_t ChronosMock::now() const
-{
-    std::time_t dummy(1703209792);
-    return dummy;
-}
